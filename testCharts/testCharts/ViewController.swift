6e1bcbb188e6bb0557136cee862d561bf4d6ff95
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-03-03 22:56:03 +0900

    円グラフを表示する

--- testCharts/testCharts/ViewController.swift
+++ testCharts/testCharts/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pieChartsView: PieChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.pieChartsView.usePercentValuesEnabled = true
+        self.pieChartsView.centerText = "テストデータ"
+        
+        let dataEntries = [
+            PieChartDataEntry(value: 40, label: "A"),
+            PieChartDataEntry(value: 35, label: "B"),
+            PieChartDataEntry(value: 25, label: "C")
+        ]
+        
+        let dataSet = PieChartDataSet(values: dataEntries, label: "データの割合")
+        dataSet.colors = ChartColorTemplates.vordiplom()
+        dataSet.valueTextColor = UIColor.black
+        dataSet.entryLabelColor = UIColor.black
+        
+        self.pieChartsView.data = PieChartData(dataSet: dataSet)
+        
+        view.addSubview(self.pieChartsView)
     }
 
 
